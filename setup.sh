f72ef7a5225fe161f1738902268991d32b6a5be7
Ryan Valverde de Ward <ryandward@gmail.com>
2023-09-17 11:08:47 -0500

    add: gnome terminal settings

--- setup.sh
+++ setup.sh
@@ -1,1 +1,18 @@
-ln -sf /home/ryandward/dotfiles/.zshrc ~/.zshrc
+#!/bin/bash
+
+DOTFILES_DIR=$(dirname "$(realpath "$0")")
+
+# Create directories if they don't exist
+mkdir -p ~/.config/nvim
+
+# Create symlinks
+ln -sf $DOTFILES_DIR/.bash_profile ~/.bash_profile
+ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
+ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
+ln -sf $DOTFILES_DIR/init.vim ~/.config/nvim/init.vim
+
+# Load GNOME Terminal settings
+dconf load /org/gnome/terminal/legacy/profiles:/ < $DOTFILES_DIR/gnome-terminal-settings.dconf
+
+echo "Dotfiles and GNOME Terminal settings linked."
+
