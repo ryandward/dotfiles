3c8ec9661930b072ccdf8153816cee7bda9f5e54
Ryan Ward <ryandward@gmail.com>
2023-09-27 12:33:42 -0500

    add: backup logic

--- setup.sh
+++ setup.sh
@@ -5,14 +5,28 @@ DOTFILES_DIR=$(dirname "$(realpath "$0")")
 # Create directories if they don't exist
 mkdir -p ~/.config/nvim
 
-# Create symlinks
-ln -sf $DOTFILES_DIR/.bash_profile ~/.bash_profile
-ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
-ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
-ln -sf $DOTFILES_DIR/init.vim ~/.config/nvim/init.vim
+# Backup and create symlinks
+for file in .bash_profile .vimrc .zshrc p10k.zsh init.vim; do
+  target=~
+  if [ "$file" == "init.vim" ]; then
+    target=~/.config/nvim
+  fi
+  
+  if [ -f "$target/$file" ]; then
+    cp "$target/$file" "$DOTFILES_DIR/$file.bak"
+  fi
+
+  ln -sf $DOTFILES_DIR/$file $target/$file
+done
+
+# Backup GNOME Terminal settings
+dconf dump /org/gnome/terminal/legacy/profiles:/ > "$DOTFILES_DIR/gnome-terminal-settings.dconf.bak"
 
 # Load GNOME Terminal settings
 dconf load /org/gnome/terminal/legacy/profiles:/ < $DOTFILES_DIR/gnome-terminal-settings.dconf
 
 echo "Dotfiles and GNOME Terminal settings linked."
+
+# Install Vim-Plug for Neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
